ca4d90917b81ee72164029de1e864413563d3c71
Bruno Azuara <bruno.azuara@gmail.com>
2022-11-21 19:56:50 +0100

    add rubocop

--- lua/bazuara/plugins/lsp/null-ls.lua
+++ lua/bazuara/plugins/lsp/null-ls.lua
@@ -17,6 +17,7 @@ null_ls.setup({
 		--  to disable file types use
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 		formatting.prettier, -- js/ts formatter
+		null_ls.builtins.diagnostics.rubocop, -- rubocop for ruby
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
